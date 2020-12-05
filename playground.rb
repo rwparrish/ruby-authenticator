b0a91493b3c8731e93d64e27f1f4d4a1fe6294fb
rwparrish <rino3717@gmail.com>
2020-12-05 11:28:46 -0700

    first commit

---
+++ playground.rb
@@ -0,0 +1,12 @@
+users = [
+  { username: "Ryan", password: "1" },
+  { username: "Maria", password: "1" },
+  { username: "Adette", password: "1" },
+  { username: "Zoë", password: "1" }
+]
+
+puts "Welcome to the AUTHENTICATOR"
+25.times { print "-" }
+puts 
+puts "This program will take input from the use and compare the password"
+puts "If the password is correct, you will get back the user object"
