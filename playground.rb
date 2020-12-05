214d2b1a62e28ba54fb55cc7d134fb67c2c5a9f3
rwparrish <rino3717@gmail.com>
2020-12-05 11:54:14 -0700

    user iteration working

--- playground.rb
+++ playground.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 users = [
   { username: "Ryan", password: "1" },
   { username: "Maria", password: "2" },
@@ -13,6 +15,17 @@ puts "If the password is correct, you will get back the user object"
 
 attempts = 1
 while attempts < 4
+  puts "Username: "
+  username = gets.chomp
+  puts "Password: "
+  password = gets.chomp
+  users.each do |user|
+   if user[:username] == username && user[:password] == password
+    puts user
+   else 
+    puts "Are you a lousy hacker?"
+   end
+  end 
   puts "Press 'n' to quit or any other hey to continue: "
   input = gets.chomp.downcase.strip
   break if input == "n"
